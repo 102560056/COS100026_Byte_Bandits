4ea85c3b6e41d7b73d34afd099cfef7f5ca2bd57
102560056 <102560056@student.swin.edu.au>
2023-05-24 13:36:33 +1000

    Header and Manage Progress

---
+++ eoi.php
@@ -0,0 +1,26 @@
+<tr>
+                    <td>asdfg</td>
+                    <td>qwefwsdf</td>
+                    <td>yudfgbsd</td>
+                    <td>19/05/2022</td>
+                    <td>Male</td>
+                    <td>1323 asdkjf</td>
+                    <td>VIC</td>
+                    <td>3629</td>
+                    <td>asldfjkh@mail.com</td>
+                    <td>0468732198</td>
+                    <td>Python,PHP,C++,MYSQL</td>
+                    <td>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni quas repellat tempora, laboriosam
+                        tempore consectetur culpa facere assumenda repellendus fugiat ullam! Quae enim dolore quam porro
+                        earum tenetur quod vitae, fugiat iure repellat at vero! Necessitatibus harum ratione eaque quia
+                        quae
+                        iure asperiores fugit deserunt vitae vero aliquid cum explicabo obcaecati, repellat maiores sint
+                        libero dolorum consequatur praesentium reiciendis mollitia quaerat. Aspernatur ipsam delectus
+                        vitae
+                        sunt, esse quas alias cumque numquam nisi quam quis fuga officiis quo saepe magnam voluptatibus
+                        unde
+                        reprehenderit molestias placeat eaque? Nihil, tempore quos? Adipisci temporibus velit impedit
+                        eum
+                        nemo neque harum incidunt at facere minus.</td>
+                    <td class="status" value="New">New</td>
+                </tr>
